f03bf7791679678fb229016206b96639ee84076d
j-jonesn <jgjones2s@proton.me>
2026-07-23 09:35:30 -0500

    Export HP Tuners importer API

--- src/tune_assistant/importers/__init__.py
+++ src/tune_assistant/importers/__init__.py
@@ -1,1 +1,19 @@
-"""Log importers."""
+"""HP Tuners log importers."""
+
+from tune_assistant.importers.hptuners_csv import (
+    ChannelMetadata,
+    HPTunersCSVError,
+    HPTunersLog,
+    ImportDiagnostics,
+    import_hptuners_csv,
+    load_channel_config,
+)
+
+__all__ = [
+    "ChannelMetadata",
+    "HPTunersCSVError",
+    "HPTunersLog",
+    "ImportDiagnostics",
+    "import_hptuners_csv",
+    "load_channel_config",
+]
